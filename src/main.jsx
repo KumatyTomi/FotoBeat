738c1634e0471aa3ae62acb2e3142c485e3e0bae
zalson8312-web <zalson8312@gmail.com>
2026-06-08 09:58:47 +0200

    Wrap app with safety boundary

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
+import { AppBoundary } from './components/AppBoundary.jsx';
 import './styles.css';
 import './render-preview.css';
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <AppBoundary>
+      <App />
+    </AppBoundary>
   </React.StrictMode>
 );
