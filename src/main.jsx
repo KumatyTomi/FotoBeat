3f2861cd21e0747af85da014a698ad5cb82e88b1
KumatyTomi <kuzapatrycja8@gmail.com>
2026-06-10 01:26:08 +0200

    Import Tailwind entrypoint

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
 import { AppBoundary } from './components/AppBoundary.jsx';
 import ShellContainer from './components/shell/ShellContainer.jsx';
+import './tailwind.css';
 import './styles.css';
 import './render-preview.css';
 import './desktop-render.css';
